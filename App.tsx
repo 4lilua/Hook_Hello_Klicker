4636405c6486647f275ae5cd0d82c80e65b2a89f
4lilua <161029480+4lilua@users.noreply.github.com>
2025-07-08 12:07:51 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
-/*Grupo: Aline Silvério Mendes e Luana de Carvalho Bonfim*/
+/*Grupo: Aline Silvério Mendes e Luana de Carvalho Bomfim*/
 
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Home from './src/screens/home/Home';
 
+//Função que abre o aplicativo na tela inicial (Home)
 export default function App() {
   return (
     <Home />
